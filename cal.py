91dc753e87c6a772d135d1776ac96227e29e4b50
raahulhm <rahulhm1999@gmail.com>
2022-02-14 17:24:50 +0530

    add conditional output

--- cal.py
+++ cal.py
@@ -10,6 +10,7 @@ def add(x, y):
 
 
 a = input("enter your first number:")
-a = input("enter your second number:")
+b = input("enter your second number:")
+
 
 print("the sum is {}".format(add(a, b)))
